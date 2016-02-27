b48cd49c94d00ffb6d25c8d37a402b40bd1f170e
kfzx-qiusd <qiusd@sdc.icbc.com.cn>
2016-02-27 23:03:45 +0800

    新增其他页面、js、css 完成点击导航加载其他页面

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-//切换网页，装在到指定id的div中
-var ajaxobj=new AJAXRequest;    // 创建AJAX对象,类在刚刚那个文件里了
-ajaxobj.method="GET";   // 设置请求方式为GET
-ajaxobj.url="record.html";  // 响应的URL,以后可以改为一些动态处理页,这在页里可以有目的返回不同的数据
-ajaxobj.callback=function(xmlobj) { // 设置回调函数，输出响应内容
-     document.getElementById('middleDivDes').innerHTML = xmlobj.responseText;    
+// 绑定列表点击事件，点击后会传递event给绑定的时间
+var navList = document.getElementById('nav-pills');
+if (navList) {
+    navList.addEventListener('click', navSelect, false);
+};
+function navSelect(event) {
+    var selectedBtn = event.target;             // click事件对象
+    var pageUrl = "./pages/"+selectedBtn.getAttribute('urltag');
+    console.log(selectedBtn.innerHTML+": 加载:"+pageUrl);
+    loadPage(pageUrl,'middleDivDes');
 }
-ajaxobj.send();    // 发送请求
+//装页面到指定id的div中
+function loadPage(pageUrl,divId){
+    var ajaxobj = new AJAXRequest; // 创建AJAX对象,类在刚刚那个文件里了
+    ajaxobj.method = "GET";   // 设置请求方式为GET
+    ajaxobj.url = pageUrl;  // 响应的URL,以后可以改为一些动态处理页,这在页里可以有目的返回不同的数据
+    ajaxobj.callback = function(xmlobj) { // 设置回调函数，输出响应内容
+         // document.getElementById('middleDivDes').innerHTML = xmlobj.responseText;    
+         document.getElementById(divId).innerHTML = xmlobj.responseText; 
+    }
+    ajaxobj.send();    // 发送请求
+}
