8f5328c7b5936455e6722f1b6fca389e93e1d3a6
kfzx-qiusd <qiusd@sdc.icbc.com.cn>
2016-02-29 21:36:13 +0800

    test1

--- js/myabbs.js
+++ js/myabbs.js
@@ -8,10 +8,19 @@ var RGBChange = function() {
     // $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
     console.log("sliding....");
 };
-var g = $('#G').slider()
+var bodyabbs1 = $('#myabbs1').slider()
         .on('slide', RGBChange)
         .data('slider');
-g.setValue(51);
+var bodyabbs2 = $('#myabbs2').slider()
+        .on('slide', RGBChange)
+        .data('slider');
+var bodyabbs3 = $('#myabbs3').slider()
+        .on('slide', RGBChange)
+        .data('slider');
+var bodyabbs4 = $('#myabbs4').slider()
+        .on('slide', RGBChange)
+        .data('slider');                        
+// g.setValue(51);
 console.log(g.getValue());
 // var g1 = $('#G').setValue(50);
 // alert(1);
